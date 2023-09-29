7454189baa29ef300737ea14a3f2a1508d563a1a
Kadiiravsaar <kadiiravsar@gmail.com>
2023-09-29 15:02:08 +0300

    swagger'e authorize butonu eklemek

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -21,33 +21,33 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen(c =>
-//{
-//    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Test01", Version = "v1" });
-//    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-//    {
-//        Name = "Authorization",
-//        Type = SecuritySchemeType.ApiKey,
-//        Scheme = "Bearer",
-//        BearerFormat = "JWT",
-//        In = ParameterLocation.Header,
-//        Description = "JWT Authorization header using the Bearer scheme."
-//    });
-//    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-//                {
-//                    {
-//                          new OpenApiSecurityScheme
-//                          {
-//                              Reference = new OpenApiReference
-//                              {
-//                                  Type = ReferenceType.SecurityScheme,
-//                                  Id = "Bearer"
-//                              }
-//                          },
-//                         new string[] {}
-//                    }
-//                });
-//});
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Test01", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme."
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                          new OpenApiSecurityScheme
+                          {
+                              Reference = new OpenApiReference
+                              {
+                                  Type = ReferenceType.SecurityScheme,
+                                  Id = "Bearer"
+                              }
+                          },
+                         new string[] {}
+                    }
+                });
+});
 
 
 #region JWT
