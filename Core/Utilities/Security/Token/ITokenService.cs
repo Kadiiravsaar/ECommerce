362869ad9407ef5ce198adeaa52c6b04d779c919
Kadiiravsaar <kadiiravsar@gmail.com>
2023-10-03 12:35:55 +0300

    [017] JWT'nın Core Katmanına Taşınması

---
+++ Core/Utilities/Security/Token/ITokenService.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Utilities.Security.Token
+{
+    public interface ITokenService
+    {
+        AccessToken CreateToken(int userId, string userName);
+
+    }
+}
---
+++ Core/Utilities/Security/Token/JWT/TokenService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Core.Utilities.Security.Token.JWT
+{
+    public class TokenService : ITokenService
+    {
+        private readonly AppSettings _appSettings;
+
+        public TokenService(IOptions<AppSettings> appSettings)
+        {
+
+            _appSettings = appSettings.Value;
+        }
+
+        public AccessToken CreateToken(int userId, string userName)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var key = Encoding.ASCII.GetBytes(_appSettings.SecurityKey);
+
+            var tokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier,userId.ToString())  ,   //                 burası token içeriği
+                    new Claim(ClaimTypes.Name,userName)
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+
+            var token = tokenHandler.CreateToken(tokenDescriptor); // token oluşturucaz
+
+            return new AccessToken()
+            {
+                Token = tokenHandler.WriteToken(token),
+                UserName = userName,
+                Expiration = (DateTime)tokenDescriptor.Expires,
+                UserId = userId
+            };
+
+        }
+    }
+}
