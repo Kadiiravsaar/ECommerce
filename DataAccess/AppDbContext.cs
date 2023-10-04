b778dfb1c263c39f8f05c1abf7126d5ed9ab3332
Kadiiravsaar <kadiiravsar@gmail.com>
2023-10-04 19:21:38 +0300

    Migration hatası için revize edildi ve çalıştırıldı

--- DataAccess/AppDbContext.cs
+++ DataAccess/AppDbContext.cs
@@ -11,20 +11,37 @@ namespace DataAccess
 {
     public class AppDbContext : DbContext
     {
+        public AppDbContext()
+        {
+        }
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
             //this.Database.SetCommandTimeout(999999);
         }
 
-        public AppDbContext()
-        {
-        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    string connString = "Data Source=DESKTOP-G34LNC7;Initial Catalog=ECommerceTest;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        //    optionsBuilder.UseSqlServer(connString);
+        //}
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            string connString = "Data Source=DESKTOP-G34LNC7;Initial Catalog=ECommerceTest;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-            optionsBuilder.UseSqlServer(connString);
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-G34LNC7;Initial Catalog=ECommerceTest;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
         }
-      
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+
+        //    modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        //    tüm assembly seviyesinde configurationları tarar ve bulur
+
+
+        //    base.OnModelCreating(modelBuilder);
+        //}
+
+
 
         public DbSet<User> Users { get; set; }
 
