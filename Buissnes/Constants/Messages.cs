a61c571933e5f18f6cd08657d48eb4e8e15289e3
Kadiiravsaar <kadiiravsar@gmail.com>
2023-10-05 17:11:38 +0300

    Messagess added

--- Buissnes/Constants/Messages.cs
+++ Buissnes/Constants/Messages.cs
@@ -13,5 +13,9 @@ namespace Buissnes.Constants
         public const string Updated = "Güncellendi";
         public const string Listed = "Listelendi";
         public const string NotListed = "Listelenemedi";
-    }
+        public const string NotUser = "Kullanıcı bulunamadı";
+        public const string SuccesLogin = "Giriş Başarılı";
+        public const string FailLogin = "Giriş Başarısız";
+
+    }   
 }
