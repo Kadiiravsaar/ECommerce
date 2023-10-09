3c7563fc3c922cfffe93b87b49beb71e8b360854
Kadiiravsaar <kadiiravsar@gmail.com>
2023-10-09 12:08:43 +0300

    [024] ApiService oluşturulması

--- WebAPIWithCoreMvc/Areas/Admin/Controllers/AuthController.cs
+++ WebAPIWithCoreMvc/Areas/Admin/Controllers/AuthController.cs
@@ -1,13 +1,50 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Entitites.Dtos.Auth;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using WebAPIWithCoreMvc.ApiServices.Interfaces;
 
 namespace WebAPIWithCoreMvc.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class AuthController : Controller
     {
-        public IActionResult Index()
+        IAuthApiService _authApiService;
+        IHttpContextAccessor _contextAccessor;
+
+        public AuthController(IAuthApiService authApiService, IHttpContextAccessor contextAccessor)
+        {
+            _authApiService = authApiService;
+            _contextAccessor = contextAccessor;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginDto loginDto)
+        {
+            var user = await _authApiService.LoginAsync(loginDto);
+            if (user.Success)
+            {
+                _contextAccessor.HttpContext.Session.SetString("token", user.Data.Token);
+                var userClaims = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
+                userClaims.AddClaim(new Claim(ClaimTypes.NameIdentifier,user.Data.Id.ToString()));
+                userClaims.AddClaim(new Claim(ClaimTypes.Name,user.Data.UserName));
+                var claimPrincipal = new ClaimsPrincipal(userClaims);
+                var authProperties = new AuthenticationProperties() { IsPersistent = loginDto.IsRememberMe};
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimPrincipal, authProperties);
+                return RedirectToAction("Index", "User", new { area = "Admin" });
+            }
+            else
+            {
+                ModelState.AddModelError("", "Kullanıcı adı veya şifre hatalı");
+            }
+            return View(loginDto);
         }
     }
 }
--- WebAPIWithCoreMvc/Program.cs
+++ WebAPIWithCoreMvc/Program.cs
@@ -1,8 +1,38 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using WebAPIWithCoreMvc.ApiServices;
+using WebAPIWithCoreMvc.ApiServices.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddHttpClient();
+ 
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddSession(); 
+
+#region httpclient
+builder.Services.AddHttpClient<IAuthApiService, AuthApiService>(opt =>
+{
+    opt.BaseAddress = new Uri("https://localhost:7258/api/");
+});
+
+builder.Services.AddHttpClient<IUserApiService, UserApiService>(opt =>
+{
+    opt.BaseAddress = new Uri("https://localhost:7258/api/");
+});
+#endregion
+
+#region Cookie
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, opt =>
+{
+    opt.LoginPath = "/Admin/Auth/Login";
+    opt.ExpireTimeSpan = TimeSpan.FromDays(60);
+    opt.SlidingExpiration = true;
+    opt.Cookie.Name = "mvccookie";
+});
+
+#endregion
 
 
 var app = builder.Build();
@@ -15,6 +45,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseSession();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
