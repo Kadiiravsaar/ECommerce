ac1910984a5c12c6ab2eaf3da95ae11ee393bc3b
Kadiiravsaar <kadiiravsar@gmail.com>
2023-10-09 12:08:33 +0300

    [024] ApiService oluşturulması

---
+++ WebAPIWithCoreMvc/ApiServices/Interfaces/IAuthApiService.cs
@@ -0,0 +1,11 @@
+﻿using Core.Utilities.Response;
+using Entitites.Dtos.Auth;
+using Entitites.Dtos.User;
+
+namespace WebAPIWithCoreMvc.ApiServices.Interfaces
+{
+    public interface IAuthApiService
+    {
+        Task<ApiDataResponse<UserDto>> LoginAsync(LoginDto loginDto);
+    }
+}
---
+++ WebAPIWithCoreMvc/ApiServices/UserApiService.cs
@@ -0,0 +1,28 @@
+﻿using Core.Utilities.Response;
+using Entitites.Dtos.User;
+using WebAPIWithCoreMvc.ApiServices.Interfaces;
+
+namespace WebAPIWithCoreMvc.ApiServices
+{
+    public class UserApiService : IUserApiService
+    {
+        HttpClient _httpClient;
+
+        public UserApiService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<UserDetailDto>> GetListAsync()
+        {
+            var response = await _httpClient.GetAsync("Users/GetList");
+            if (!response.IsSuccessStatusCode)
+            {
+                return null;
+            }
+            var responseSucces = await response.Content.ReadFromJsonAsync<ApiDataResponse<IEnumerable<UserDetailDto>>>();
+            return responseSucces.Data.ToList();
+        
+        }
+    }
+}
